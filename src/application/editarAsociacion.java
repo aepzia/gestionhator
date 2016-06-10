57491e3013e50c2b07522e3a6e3b3511d30d7f88
Aizpea Babaze <aizpea_babaze@hotmail.com>
2016-06-10 12:07:47 +0200

    .fxml .java bihurtuta

---
+++ src/application/editarAsociacion.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class editarAsociacion extends Application {
+	
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+            Parent page;
+			try {
+				page = FXMLLoader.load(getClass().getResource("editarAsociacion.fxml"));
+				Scene scene = new Scene(page);
+		        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.setTitle("Editar ascoiación");
+				primaryStage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+           
+    }
+}
+
---
+++ src/application/sociosActividad.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class sociosActividad extends Application {
+	
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+            Parent page;
+			try {
+				page = FXMLLoader.load(getClass().getResource("sociosActividad.fxml"));
+				Scene scene = new Scene(page);
+		        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+				primaryStage.setScene(scene);
+				primaryStage.setTitle("Actividades del socio");
+				primaryStage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+           
+    }
+}
+
