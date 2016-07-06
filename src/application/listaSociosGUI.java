76d063f94a57208689dec5e91d295f79e256851b
Aizpea Babaze <aizpea_babaze@hotmail.com>
2016-07-06 12:41:18 +0200

    Bazkideen listako bilatzailea lortuta!

--- src/application/listaSociosGUI.java
+++ src/application/listaSociosGUI.java
@@ -7,14 +7,19 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -31,7 +36,8 @@ public class listaSociosGUI extends Application {
 	@FXML private ListView<Socio> listSocios;
 	@FXML private TextArea textInformazioa;
 	@FXML private ChoiceBox btnOrderBy;
-	
+	@FXML private TextField textBuscar;
+	ObservableList<Socio> lag ;
 	private static Socio aukera;
 	private static Stage pStage;
 	
@@ -98,8 +104,8 @@ public class listaSociosGUI extends Application {
     @FXML protected void initialize(){
 
     	try {
-			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
-			Connection conn = DriverManager.getConnection("jdbc:ucanaccess://C:/Asoziazioko_datuak/database.mdb;memory=false");
+    		Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:C:\\Asoziazioko_datuak\\datuBasea", "", "" );
 			Statement stmt = conn.createStatement();
 			String sql = "SELECT * FROM socio";
 			ResultSet rs =  stmt.executeQuery(sql);
@@ -108,22 +114,58 @@ public class listaSociosGUI extends Application {
             {
 				Controler.socioKop++; 
 				Socio s = new Socio(rs);
-				sql = "SELECT  * FROM socioActividad where socioApuntado='"+rs.getObject("DNI")+"' AND pagado='false'";
-				ResultSet rs2 =  stmt.executeQuery(sql);
-				if (rs2 !=null) {
-					
-				}
 				listSocios.getItems().add(s);
 
             }
-			
+			lag = listSocios.getItems();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+    	textBuscar.textProperty().addListener(new ChangeListener<String>() {
+    	    @Override
+    	    public void changed(ObservableValue<? extends String> observable,
+    	            String oldValue, String newValue) {
+
+    	    	handleSearchByKey(oldValue, (String)newValue);
+    	    }
+    	});
     	
     }
+    public void handleSearchByKey(String oldVal, String newVal) {
+        // If the number of characters in the text box is less than last time
+        // it must be because the user pressed delete
+        if ( oldVal != null && (newVal.length() < oldVal.length()) ) {
+            // Restore the lists original set of entries 
+            // and start from the beginning
+            listSocios.setItems( lag );
+        }
+         
+        // Break out all of the parts of the search text 
+        // by splitting on white space
+        String[] parts = newVal.toUpperCase().split(" ");
+ 
+        // Filter out the entries that don't contain the entered text
+        ObservableList<Socio> subentries = FXCollections.observableArrayList();
+        for ( Socio entry: listSocios.getItems() ) {
+            boolean match = true;
+            String entryText = entry.toString();
+            for ( String part: parts ) {
+                // The entry needs to contain all portions of the
+                // search string *but* in any order
+                if ( ! entryText.toUpperCase().contains(part) ) {
+                    match = false;
+                    break;
+                }
+            }
+ 
+            if ( match ) {
+                subentries.add(entry);
+            }
+        }
+        listSocios.setItems(subentries);
+    }
+ 
 }
 
 
