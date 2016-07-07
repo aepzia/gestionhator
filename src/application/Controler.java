ec4e9630344f4c06737f039ba807eb56d29b44c9
Aizpea Babaze <aizpea_babaze@hotmail.com>
2016-07-07 13:20:25 +0200

    Editatzeko, datuak jartzea falta

--- src/application/Controler.java
+++ src/application/Controler.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Controler {
 	public static Socio autatutakoBazkidea;
+	public static Actividad autatutakoEkintza;
+
 	public static List<Socio> listaSocio;
 	public static int socioKop;
 	public static int actiKop;
--- src/application/editarActividadGUI.java
+++ src/application/editarActividadGUI.java
@@ -1,6 +1,13 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.hsqldb.HsqlException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,28 +16,69 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class editarActividadGUI extends Application {
 	
 	@FXML ChoiceBox<String> btnTipo;
-	@FXML ChoiceBox<String> btnBergiralea;
+	@FXML ChoiceBox<String> btnBegiralea;
+	@FXML ChoiceBox<String> hasiOr;
+	@FXML ChoiceBox<String> hasiMin;
+	@FXML ChoiceBox<String> bukOr;
+	@FXML ChoiceBox<String> bukMin;
 	@FXML Button btnAtras;
+	@FXML Button btnGuardar;
 	@FXML TextField textPrezioa;
 	@FXML TextField textPlazaKop;
-	@FXML TextField textOrHasi;
-	@FXML TextField textOrBuk;
-	
+	@FXML DatePicker hasData;
+	@FXML DatePicker bukData;
+	@FXML TextField textNombre;
 	
 	private static Stage pStage;
-	
+	private static Actividad aukera;
 	@FXML private void atras(){
-			//TODO ver asocioacion
+		listaActividadesGUI w = new listaActividadesGUI();
+		w.start(getPrimaryStage());
 	}
 	@FXML private void guardar(){
-		//TODO Ekintzako datuak datu basean gorde
-		
+		try
+        {
+			aukera = Controler.autatutakoEkintza;
+			String horaIni = hasiOr.getSelectionModel().getSelectedItem()+":"+ hasiMin.getSelectionModel().getSelectedItem();
+			String horaFin = bukOr.getSelectionModel().getSelectedItem()+":"+ bukMin.getSelectionModel().getSelectedItem();
+			Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:C:\\Asoziazioko_datuak\\datuBasea", "", "" );
+    		Statement stmt = conn.createStatement();
+    		int id = Controler.actiKop +1;
+    		//TODO monitore clasea sortzean, onen DNIa gordeko da.
+    		String sql = "UPDATE actividad SET "
+    				+ "id='"
+            		+ id
+            		+"',tpActividad='"+btnTipo.getSelectionModel().getSelectedItem()+"',monitorDNI='"+btnBegiralea.getSelectionModel().getSelectedItem()+"',fechaIni='"+hasData.getValue()
+            		+"',fechaFin='"+bukData.getValue()+"',horaIni='"+horaIni +"',horaFin='"+horaFin
+            		+"',nombre='"+textNombre.getText()+
+            		"',numero_de_plazas='"+textPlazaKop.getText()+"',precio='"+textPrezioa.getText() + "WHERE id='"+aukera.getId()+"'";
+    		System.out.println(bukData.getValue());
+    		System.out.println(sql);
+    		stmt.executeUpdate(sql);
+            stmt.close();    	
+            conn.close();
+        
+        } 
+		catch ( Exception e ){
+			e.printStackTrace();
+			if (e instanceof NullPointerException){
+				//TODO sartutako datu hutsak tratatu
+			}
+			if(e instanceof HsqlException){
+				//TODO DNI errepikatua
+			}
+        }		
+
+		System.out.println("gorde da");
+	
 	}
 	
 	public static Stage getPrimaryStage() {
@@ -38,7 +86,7 @@ public class editarActividadGUI extends Application {
     }
 
     private void setPrimaryStage(Stage pStage) {
-        editarActividadGUI.pStage = pStage;
+       editarActividadGUI.pStage = pStage;
     }   
     
     public static void main(String[] args) {
@@ -50,12 +98,11 @@ public class editarActividadGUI extends Application {
             Parent page;
 			try {
 				setPrimaryStage(primaryStage);
-				page = FXMLLoader.load(getClass().getResource("../itxura/editarActividad.fxml"));
+				page = FXMLLoader.load(getClass().getResource("../itxura/nuevaActividad.fxml"));
 				Scene scene = new Scene(page);
 		        scene.getStylesheets().add(getClass().getResource("../itxura/style.css").toExternalForm());
 				primaryStage.setScene(scene);
-				primaryStage.setTitle("Editar ascoiación");
-				primaryStage.setFullScreen(true);
+				primaryStage.setTitle("Nueva actividad");
 				primaryStage.show();
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -63,11 +110,10 @@ public class editarActividadGUI extends Application {
 			
            
     }
-    @FXML
-    protected void initialize(){
-    	System.out.println("IHFDU");
-    	//TODO datu basetik hartu eta datuak idatzi 
- 
+    @FXML protected void initialize(){
+    	//TODO monitore aukera datu basetik osatu 
+    	//TODO datu guztiak ezarri pantailan
+    	
     	
     }
 }
