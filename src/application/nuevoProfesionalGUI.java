95ce4397190de0f6edfe14f1dc3f7499d42fd025
Aizpea Babaze <aizpea_babaze@hotmail.com>
2016-07-12 13:38:38 +0200

    Profesional berria sortzeko

---
+++ src/application/nuevoProfesionalGUI.java
@@ -0,0 +1,107 @@
+package application;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class nuevoProfesionalGUI extends Application {
+	
+	@FXML TextField textEmpresa;
+	@FXML TextField textCIF;
+	@FXML TextField textTel1;
+	@FXML TextField textDirec;
+	@FXML TextField textNombre;
+	@FXML TextField textNAN;
+	@FXML TextField textTel2;
+	@FXML Button btnAtras;
+	@FXML Button btnGuardar;
+	
+
+	private static Stage pStage;
+
+	@FXML public void guardar(){
+		Connection conn;
+		try {
+			conn = DriverManager.getConnection("jdbc:h2:C:\\Asoziazioko_datuak\\datuBasea", "", "" );
+			Statement stmt = conn.createStatement();
+			//TODO monitore clasea sortzean, onen DNIa gordeko da.
+			String sql = "INSERT INTO profesional "
+					+ "VALUES("
+	        		+ "'"+textEmpresa.getText()+"',"
+	        		+ "'"+textDirec.getText()+"',"
+	        		+ "'"+textTel1.getText()+"',"
+	        		+ "'"+textNombre.getText()+"',"
+	        		+ "'"+textTel2.getText()+"',"
+	        		+ "'"+textNAN.getText()+"',"
+	        		+ "'"+textCIF.getText()
+	        		+ "')";
+			System.out.println(sql);
+			stmt.executeUpdate(sql);
+	        stmt.close();    	
+	        conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	@FXML public void atras(){
+		listMonitorGUI w = new listMonitorGUI();
+		w.start(getPrimaryStage());
+	}
+	
+	public static Stage getPrimaryStage() {
+        return pStage;
+    }
+
+    private void setPrimaryStage(Stage pStage) {
+        nuevoProfesionalGUI.pStage = pStage;
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage){
+    	AnchorPane page;
+			try {
+				setPrimaryStage(primaryStage);
+
+				page = (AnchorPane) FXMLLoader.load(getClass().getResource("../itxura/nuevoProfesional.fxml"));
+				Scene scene = new Scene(page);
+		        scene.getStylesheets().add(getClass().getResource("../itxura/style.css").toExternalForm());
+		        
+		        primaryStage.setScene(scene);
+				primaryStage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+    }
+    @FXML protected void initialize(){
+
+    	
+    }
+    
+ 
+}
+
+
